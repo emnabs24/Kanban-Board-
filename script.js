836fff9c59b0fb5ba6a3a3a8bf9eb720db29457a
Emma24968 <emnabs12@gmail.com>
2025-08-02 16:51:22 +0100

    I created a function to generate  tasks element dynamically

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const modal = document.querySelector(".confirm-modal");
 const columnsContainer = document.querySelector(".columns");
 const columns = columnsContainer.querySelectorAll(".column");
+
+const tasksElaments = (content) => {
+    const task = document.createElement("div");
+    task.classList.add("task");
+    task.innerHTML = `
+        <div class="task-content">${content}</div>
+    `;
+        return task;
+    }
